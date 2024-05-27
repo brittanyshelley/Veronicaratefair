7579b32cb9ad426e51a5038eb331b6ae4af6fc46
brittanyshelley <brittany.j.shelley@gmail.com>
2024-05-26 21:10:07 -0600

    refactor: Update LogoClouds component with new logos and improved styling

--- src/components/LogoClouds.js
+++ src/components/LogoClouds.js
@@ -10,7 +10,7 @@ export default function LogoClouds() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <Image
             className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
-            src="/rmg.png"
+            src="/strive.png"
             alt="Transistor"
             width={158}
             height={48}
@@ -23,16 +23,16 @@ export default function LogoClouds() {
             height={48}
           />
           <Image
-            className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
-            src="/mcap.svg"
-            alt="Tuple"
+            className="col-span-2 max-h-24 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src="/TD.png"
+            alt="SavvyCal"
             width={158}
             height={48}
           />
           <Image
-            className="col-span-2 max-h-24 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="/TD.png"
-            alt="SavvyCal"
+            className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
+            src="/home-trust.svg"
+            alt="Tuple"
             width={158}
             height={48}
           />
